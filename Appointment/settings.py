dd5782dfd4b402b3bfa326102eea1c7816fb69b2
unknown <tomararpit9@gmail.com>
2024-10-04 19:26:13 +0530

    code

--- Appointment/settings.py
+++ Appointment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--e*uhjo6k6b7q2jp#*-th^24bcx2p3(_n@y)hqkpxn!eqb-k2e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
