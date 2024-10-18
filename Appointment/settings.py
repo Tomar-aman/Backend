284070088cc282cd9b946ef8ea47c4f8316fa776
unknown <tomararpit9@gmail.com>
2024-10-18 19:25:46 +0530

    server database

--- Appointment/settings.py
+++ Appointment/settings.py
@@ -92,13 +92,23 @@ WSGI_APPLICATION = 'Appointment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres.srjdwvpvfrsdhzsekftz',
+        'PASSWORD': '9340913783@1212',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
-
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
