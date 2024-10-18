ab17526760881de4f5875bea685aaddc45a9802a
unknown <tomararpit9@gmail.com>
2024-10-18 17:45:08 +0530

    role fix

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -27,7 +27,7 @@ class HospitalClinicSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name']
+        fields = ['first_name', 'last_name','role']
 
 class DoctorProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer()
